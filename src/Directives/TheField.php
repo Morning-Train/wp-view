3d2665ab32ccfe1052e84ffc39edf00732d6554b
Mamu <mathiasmun@gmail.com>
2021-10-23 16:42:44 +0200

    Add function_exists check

--- src/Directives/TheField.php
+++ src/Directives/TheField.php
@@ -8,6 +8,10 @@ class TheField extends \Morningtrain\WP\View\Abstracts\AbstractDirective
 {
     public function handle(?string $expression = null): string
     {
+        if (!function_exists('the_field')) {
+            return '';
+        }
+
         return "<?php the_field('$expression'); ?>";
     }
 }
